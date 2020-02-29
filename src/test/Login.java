e7a0f5d3025323b35c69db4e9fb44da737d5d768
Aiganysh Tukueva <aiganyshtukueva@Aiganyshs-MacBook-Pro.local>
2020-02-29 15:10:34 -0500

    add changes

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,6 +3,6 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("User logged in ");
-        System.out.println("User inside of the loggin page... ");
+        System.out.println("User of the loggin page... ");
     }
 }
