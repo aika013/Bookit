50afd8985e99f84562cf9cf221a1faefb1dd3619
aika013 <58495031+aika013@users.noreply.github.com>
2020-02-29 15:25:20 -0500

    Update Login.java

--- src/test/Login.java
+++ src/test/Login.java
@@ -8,5 +8,7 @@ public class Login {
         System.out.println("User inside of the loggin page... ");
 
         System.out.println("from intelij");
+        
+        System.out.println("from intelij");
     }
 }
